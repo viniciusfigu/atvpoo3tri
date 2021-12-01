81782d5683a8d4d567f83e9e22e4d4663cedfa6c
viniciusfigu <79717926+viniciusfigu@users.noreply.github.com>
2021-11-30 22:13:27 -0300

    Add files via upload

---
+++ proj_3camadas"/code/DAL/Conexao.cs
@@ -0,0 +1,71 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace proj_3camadas.code.DAL
+{
+    class Conexao
+    {
+        MySqlConnection conexao;
+
+        public void Conectar()
+        {
+            try
+            {
+                string conn = "Persist Security Info = false; " +
+                              "server = localhost; " +
+                              "database = pro_3camadas; " +
+                              "uid = root; pwd=";
+
+                conexao = new MySqlConnection(conn);
+                conexao.Open();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível conectar ao banco de dados.\n" + ex.Message);
+            }
+        }
+
+        public DataTable ExecutarConsulta(string sql)
+        {
+            try
+            {
+                Conectar();
+                MySqlDataAdapter dados = new MySqlDataAdapter(sql, conexao);
+                DataTable dt = new DataTable();
+                dados.Fill(dt);
+
+                return dt;
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível executar a CONSULTA solicitada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void ExecutarComando(string sql)
+        {
+            Conectar();
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                comando.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("A instrução não foi realizada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
